f922d923c1a06a3d403e11f88229c81e0fb508fa
fidelity2851 <ukwuanifidelis2851@gmail.com>
2021-03-27 19:09:48 +0100

    initial

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,7 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<style scoped>
 
 <style>
 #app {
